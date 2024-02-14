631ae12e40b8cd929f983c7897a5e0c596aa5eb2
imran2024bd <imranitbd34@gmail.com>
2024-02-14 11:01:11 +0600

    Get Triangle Base And Height index complete

---
+++ .vscode/script/triangle-area.js
@@ -0,0 +1,11 @@
+// console.log('Triangle inside function');
+
+/**
+ * Objective: get base, height of a triangle. Calculate the area by using the provided formula. and then display the area.
+ * step -1: get base value of the triangle
+ * step-2: added an id in the base input field
+ * step-3: use getElementById to access the input field
+ * step-4: get value from the input field. (value is string now)
+ * step-5: convert the value to a number. use parseFloat
+ * 
+ */
