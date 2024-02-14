1efa3840774f24e1d77773665ba988c9e1b847b7
imran2024bd <imranitbd34@gmail.com>
2024-02-14 23:38:36 +0600

    calculateRhombusArea by common function is done

--- script/common-calculation.js
+++ script/common-calculation.js
@@ -1,13 +1,16 @@
+
+// Run function of the area
 function calculateRhombusArea() {
     const a = getInputvalueById('rhombus-base');
     const b = getInputvalueById('rhombus-height');
-    console.log(a , b);
+    // console.log(a , b);
     const area = a * b ;
-    console.log('area is the Rombus is: ' , area);
+    console.log('Area of the Rombus is: ' , area);
+    setInnerTextById('rhombus-area', area);
 }
 
 
-
+// To find main value function of geometry property
 function getInputvalueById(inputFieldId) {
    const inputField = document.getElementById(inputFieldId);
    const inputText = inputField.value;
@@ -17,6 +20,7 @@ function getInputvalueById(inputFieldId) {
 
 }
 
+// to show area function of the diagram
 function setInnerTextById(elementId , area) {
     const element = document.getElementById(elementId);
     element.innerText = area;
