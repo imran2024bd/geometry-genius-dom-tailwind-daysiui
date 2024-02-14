1aadbc76774779abe74b7472a147cb8494c6e7b1
imran2024bd <imranitbd34@gmail.com>
2024-02-14 23:21:03 +0600

    calculateRhombusArea is done

--- script/common-calculation.js
+++ script/common-calculation.js
@@ -2,15 +2,22 @@ function calculateRhombusArea() {
     const a = getInputvalueById('rhombus-base');
     const b = getInputvalueById('rhombus-height');
     console.log(a , b);
+    const area = a * b ;
+    console.log('area is the Rombus is: ' , area);
 }
 
 
 
 function getInputvalueById(inputFieldId) {
-   const inputField = document.getElementById(inputField);
+   const inputField = document.getElementById(inputFieldId);
    const inputText = inputField.value;
    const inputNumber = parseFloat(inputText);
    console.log(inputNumber);
    return inputNumber;
 
+}
+
+function setInnerTextById(elementId , area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 }
